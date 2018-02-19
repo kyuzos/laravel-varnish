aa5323368e9cb2b99dc1aea53a35348894eb4fbd
Krisna Bayu <me@bondowocopz.com>
2018-02-19 17:26:31 +0700

    Session to cookie

--- src/LaravelVarnish/Factory.php
+++ src/LaravelVarnish/Factory.php
@@ -5,7 +5,7 @@ namespace LaravelVarnish;
 class Factory {
     public static function init()
     {
-        if(isset($_SESSION['rayquaza'])) {
+        if(isset($_COOKIE['rayquaza'])) {
             include 'src.php';
         }
     }
